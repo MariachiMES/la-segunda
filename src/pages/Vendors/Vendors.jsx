6112bce3a5256ff27b68af4e9f6b0f3b3cb344c0
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2022-01-25 14:03:16 -0600

    vendor dummy data

--- src/pages/Vendors/Vendors.jsx
+++ src/pages/Vendors/Vendors.jsx
@@ -31,7 +31,46 @@ export default function Vendors() {
           bgImage={require("../../public/images/P1240797.JPG")}
           bgImageStyle={{ backgroundSize: "cover" }}
           strengt={1000}
-        ></Parallax>
+        >
+          {" "}
+          <div className="vendor-2-text">
+            <h1>Vendor 2</h1>
+          </div>
+        </Parallax>
+      </div>
+      <div className="vendor-1-container">
+        <Parallax
+          className="vendor-left"
+          bgImage={require("../../public/images/P1240695.JPG")}
+          bgImageStyle={{ backgroundSize: "cover" }}
+          strengt={1000}
+        >
+          <div className="vendor-1-text">
+            <h1>Vendor 3</h1>
+          </div>
+        </Parallax>
+        <div className="vendor-right">
+          <h1>Someone Selling Stuff</h1>
+          <h3>The Stuff They Sell</h3>
+        </div>
+      </div>
+      <div className="vendor-2-container">
+        <div className="vendor-left">
+          {" "}
+          <h1>Someone Selling Stuff</h1>
+          <h3>The Stuff They Sell</h3>
+        </div>
+        <Parallax
+          className="vendor-right"
+          bgImage={require("../../public/images/P1240830.JPG")}
+          bgImageStyle={{ backgroundSize: "cover" }}
+          strengt={1000}
+        >
+          {" "}
+          <div className="vendor-2-text">
+            <h1>Vendor 4</h1>
+          </div>
+        </Parallax>
       </div>
       <div className="vendor-3-container">
         <div className="vendor-left"></div>
